b5abd07e3c9338507a989ce89ba9ec85fa65b6d2
Navneet Lal Gupta <navneetlalg@gmail.com>
2022-01-11 13:54:47 +0530

    add: badge styling && folder structure

--- src/index.tsx
+++ src/index.tsx
@@ -3,16 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import '@fontsource/poppins/400.css';
+import '@fontsource/poppins/500.css';
+import '@fontsource/poppins/600.css';
+import '@fontsource/poppins/700.css';
+
 import { ThemeProvider } from '@mui/material/styles';
-import {theme} from './intugine'
+import { LightTheme, DarkTheme } from './theme'
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={LightTheme}>
       <App />
     </ThemeProvider>
   </React.StrictMode>,
--- src/theme/lightTheme.ts
+++ src/theme/lightTheme.ts
@@ -1,154 +1,46 @@
 import { createTheme } from "@mui/material/styles";
+import typography from './typography.json';
 
-const typography = {
-  fontFamily: [
-    "-apple-system",
-    "BlinkMacSystemFont",
-    '"Segoe UI"',
-    "Roboto",
-    '"Helvetica Neue"',
-    "Arial",
-    "sans-serif",
-    '"Apple Color Emoji"',
-    '"Segoe UI Emoji"',
-    '"Segoe UI Symbol"',
-  ].join(","),
-  h1: {
-    fontSize: 32,
-    fontFamily: "'Poppins', sans-serif",
-    fontWeight: 700,
-    lineHeight: "48px",
-  },
-  h2: {
-    fontSize: 24,
-    fontFamily: "'Poppins', sans-serif",
-    fontWeight: 700,
-    lineHeight: "36px",
-  },
-  h3: {
-    fontSize: 20,
-    fontFamily: "'Poppins', sans-serif",
-    fontWeight: 600,
-    lineHeight: "32px",
-  },
-  h4: {
-    fontSize: 16,
-    fontFamily: "'Poppins', sans-serif",
-    fontWeight: 600,
-    lineHeight: "24px",
-  },
-  h5: {
-    fontSize: 12,
-    fontFamily: "'Poppins', sans-serif",
-    fontWeight: 500,
-    lineHeight: "20px",
-  },
-  h6: {
-    fontSize: 11,
-    fontFamily: "'Poppins', sans-serif",
-    fontWeight: 500,
-    lineHeight: "16px",
-  },
-  subtitle1: {
-    fontSize: 20,
-    fontFamily: "'Source Sans Pro', sans-serif",
-    fontWeight: 400,
-    lineHeight: "32px",
-  },
-  subtitle2: {
-    fontSize: 16,
-    fontFamily: "'Source Sans Pro', sans-serif",
-    fontWeight: 400,
-    lineHeight: "24px",
-  },
-  body1: {
-    fontSize: 14,
-    fontFamily: "'Source Sans Pro', sans-serif",
-    fontWeight: 400,
-    lineHeight: "20px",
-  },
-  body2: {
-    fontSize: 12,
-    fontFamily: "'Source Sans Pro', sans-serif",
-    fontWeight: 400,
-    lineHeight: "16px",
-  },
-  button: {
-    textTransform: "none",
-  },
-};
+import type { TypographyOptions } from "@mui/material/styles/createTypography";
 
-export const lightTheme = createTheme({
+export let lightTheme = createTheme({
   palette: {
+    mode: 'light',
     primary: {
-      // blue
       light: "#91B0FA",
       main: "#2C63E5",
-      dark: "#264CA4",
-      contrastText: "#F5F8FF",
-      200: "#BFD2FD",
-      100: "#D7E3FE",
-      600: "#264CA4",
+      dark: "#264CA4"
     },
     secondary: {
-      // gray
       light: "#F2F2F2",
       main: "#C7C7C7",
-      dark: "#666666",
-      contrastText: "#E3E3E3",
-      50: "#FFF7E5",
-      300: "#A1A1A1",
+      dark: "#666666"
     },
     error: {
-      // red
       light: "#F9D7D7",
       main: "#FF5757",
-      dark: "#CC3333",
-      contrastText: "#F9D7D7",
+      dark: "#CC3333"
     },
     warning: {
-      // orange
       light: "#FFECDB",
       main: "#F2984A",
-      dark: "#D97E2E",
-      600: "#D97F30",
-      contrastText: "#FFECDB",
+      dark: "#D97E2E"
     },
     success: {
-      // green
       light: "#C2FAEA",
       main: "#00C28B",
-      dark: "#008F66",
-      contrastText: "#C2FAEA",
-    },
-    //@ts-ignore
-    blackAndWhite: {
-      main: '#1A1A1A',
-      light: '#fff',
-    },
+      dark: "#008F66"
+    }
   },
-  //@ts-ignore
   typography: {
-    ...typography,
+    ...typography as TypographyOptions
   },
 });
 
-export const theme = createTheme({
+lightTheme = createTheme(lightTheme, {
   components: {
     MuiButton: {
       styleOverrides: {
-        contained: {
-          background: lightTheme.palette.primary.main,
-          "&:hover": {
-            background: lightTheme.palette.primary.dark,
-          },
-        },
-        text: {
-          color: lightTheme.palette.primary.main,
-          "&:hover": {
-            background: lightTheme.palette.primary.dark,
-          },
-        },
         sizeSmall: {
           minWidth: 118,
           height: 32,
@@ -172,8 +64,7 @@ export const theme = createTheme({
         },
         disabled: {
           backgroundColor: lightTheme.palette.secondary.light + " !important",
-          //@ts-ignore
-          color: lightTheme.palette.secondary[300] + " !important",
+          color: '#A1A1A1' + " !important",
         },
       },
     },
@@ -186,6 +77,7 @@ export const theme = createTheme({
           borderRadius: "50%",
           ...lightTheme.typography.body1,
         },
+        
       },
     },
     MuiBreadcrumbs: {
