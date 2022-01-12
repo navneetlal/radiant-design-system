89cfbe2126bdef509cb24ccd594e5cf17500e93e
Siddhant Sharma <siddhant@intugine.com>
2022-01-12 13:50:34 +0530

    Button component added

--- src/App.tsx
+++ src/App.tsx
@@ -16,11 +16,11 @@ import {
 } from "@mui/material";
 
 import { useState } from "react";
-import { useTheme } from "@emotion/react";
+import { useTheme } from "@mui/material";
 import Badge from "./components/Badges";
 import Chip from "./components/Chip";
 import Snackbar from "./components/Snackbar";
-import Tooltip from "./components/Tooltip";
+// import Tooltip from "./components/Tooltip";
 import Button from "./components/Button";
 const TempComponent = () => {
   return <p>badge</p>;
@@ -36,11 +36,12 @@ function App() {
     <Container maxWidth="lg">
       <Grid container>
         <Button
-          variant="outlined"
+          variant="contained"
           color="primary"
           // startIcon={<MuiAdjustIcon />}
-
-        ></Button>
+          text="
+          Testing this button"
+        />
         <Badge
           rounded={true}
           text={"4"}
@@ -85,9 +86,9 @@ function App() {
 
         <Switch checked={true} sx={{ m: 4 }} />
 
-        <Tooltip title="Sample" color="error">
+        {/* <Tooltip title="Sample" color="error">
           <span>This is a tooltip</span>
-        </Tooltip>
+        </Tooltip> */}
 
         <InputLabel sx={{ m: 4 }}>
           <span>Random Label</span>
