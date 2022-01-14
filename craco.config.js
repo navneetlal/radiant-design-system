b7ea45ec29a0e23a606866fe763534a89d71a3ab
Navneet Lal Gupta <navneetlalg@gmail.com>
2022-01-14 14:34:56 +0530

    update: entrypoint and sample craco config for future changes

--- craco.config.js
+++ craco.config.js
@@ -1,5 +1,7 @@
+const { ESLINT_MODES } = require("@craco/craco");
+
 module.exports = {
-  webpack: {
-    entryPoint: './src/server.tsx'
-  }
-}
+    eslint: {
+        mode: ESLINT_MODES.file
+    },
+};
