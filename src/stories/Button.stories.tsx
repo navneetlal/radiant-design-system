24352180c6576a2b805be15953bb351a3f957920
Kunal Pant <kunalpant@intugine.com>
2022-01-27 13:19:11 +0530

    switch stories

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -1,46 +1,45 @@
-import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
 
-import Button from '../components/Button';
+import Button from "../components/Button";
 
 export default {
-  title: 'Components/Button',
+  title: "Components/Button",
   component: Button,
   args: {
-    text: "BUTTON",
-    variant: 'contained'
-  }
+    text: "Button",
+    variant: "outlined",
+  },
 } as ComponentMeta<typeof Button>;
-
 
 const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 
 export const Primary = Template.bind({});
 
 Primary.args = {
-  color: 'primary',
+  color: "primary",
 };
 
 export const Secondary = Template.bind({});
 
 Secondary.args = {
-  color: 'secondary',
+  color: "secondary",
 };
 
 export const Success = Template.bind({});
 
 Success.args = {
-  color: 'success',
+  color: "success",
 };
 
 export const Warning = Template.bind({});
 
 Warning.args = {
-  color: 'warning',
+  color: "warning",
 };
 
 export const Error = Template.bind({});
 
 Error.args = {
-  color: 'error',
+  color: "error",
 };
