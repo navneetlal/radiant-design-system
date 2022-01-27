a3b21fd1cf2dff51f505928fdeaf8f62955051ff
Kunal Pant <kunalpant@intugine.com>
2022-01-27 13:37:25 +0530

    chip stories

---
+++ src/stories/Chip.stories.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import Chip from "../components/Chip";
+
+export default {
+  title: "Components/Chip",
+  component: Chip,
+  args: {
+    text: "Chip",
+    size: "medium",
+  },
+} as ComponentMeta<typeof Chip>;
+
+const Template: ComponentStory<typeof Chip> = (args) => <Chip {...args} />;
+
+export const OnDelete = Template.bind({});
+OnDelete.args = {
+  onDelete: true,
+};
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+  disabled: true,
+};
