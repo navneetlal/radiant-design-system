82f76cdabb8a0395e638e5b5f52831362dee8223
Navneet Lal Gupta <navneetlalg@gmail.com>
2022-01-28 14:42:25 +0530

    refactor: components/badge.tsx

--- src/components/Badge/Badge.tsx
+++ src/components/Badge/Badge.tsx
@@ -1,81 +1,72 @@
-import React, { ReactElement } from 'react';
-import { Badge as MuiBadge } from '@mui/material';
-import { useTheme } from '@mui/material';
-import { withStyles } from '@mui/styles';
+import React from 'react';
+import MuiBadge from '@mui/material/Badge';
+import { useTheme } from '@mui/material/styles';
 
-export interface IBadgeProps {
-    rounded?: boolean;
-    color?: "default" | "primary" | "secondary" | "error" | "info" | "success" | "warning";
-    text: string;
-    children?: ReactElement;
-    size: 'small' | 'medium' | 'large';
-    [key: string] : any;
+import type { BadgeProps } from '@mui/material/Badge'
 
+interface IBadgeProps extends BadgeProps {
+  rounded?: boolean;
+  color?: "primary" | "secondary" | "error" | "info" | "success" | "warning";
+  children?: React.ReactElement;
+  size?: 'small' | 'medium' | 'large';
+  [key: string]: any;
 }
 
-const Badge = ({ rounded, color, text, children, size, ...otherProps }: IBadgeProps) => {
-    const theme = useTheme()
-    return (
-        <MuiBadge
-            style={{margin : '9rem'}}
-            badgeContent={text}
-            sx={[
-                {
-                "& .MuiBadge-badge" : {
-                    //@ts-ignore
-                    backgroundColor : theme.palette[color].light,
-                    color : `1A1A1A`,
-                    borderRadius : '4px',
-                    fontFamily: '"Source Sans Pro", sans-serif',
-                }},
-                size === 'small' && {
-                    "& .MuiBadge-badge" : {
-                        height: '24px',
-                        width : '24px',
-                        transform: 'scale(1) translate(90%, -50%)',
-                        ...theme.typography.body2,
-                    }
-                },
-                size === 'medium' && {
-                    "& .MuiBadge-badge" : {
-                        height: '32px',
-                        width : '32px',
-                        transform: 'scale(1) translate(90%, -60%)',
-                        ...theme.typography.body1,
-                    }        
-                },
-                        
-                size === 'large' && {
-                    "& .MuiBadge-badge" : {
-                        height: '40px  ',
-                        width : '40px',
-                        transform: 'scale(1) translate(90%, -70%)',
-                        ...theme.typography.subtitle2,
+const Badge = ({
+  rounded = true,
+  color = "primary",
+  children,
+  size = "medium",
+  ...otherProps }: IBadgeProps) => {
+  const theme = useTheme()
+  return (
+    <MuiBadge
+      style={{ margin: '9rem' }}
+      sx={[
+        {
+          "& .MuiBadge-badge": {
+            backgroundColor: theme.palette[color].light,
+            color: `1A1A1A`,
+            borderRadius: '4px',
+            fontFamily: '"Source Sans Pro", sans-serif',
+          }
+        },
+        size === 'small' && {
+          "& .MuiBadge-badge": {
+            height: '24px',
+            width: '24px',
+            transform: 'scale(1) translate(90%, -50%)',
+            ...theme.typography.body2,
+          }
+        },
+        size === 'medium' && {
+          "& .MuiBadge-badge": {
+            height: '32px',
+            width: '32px',
+            transform: 'scale(1) translate(90%, -60%)',
+            ...theme.typography.body1,
+          }
+        },
+        size === 'large' && {
+          "& .MuiBadge-badge": {
+            height: '40px  ',
+            width: '40px',
+            transform: 'scale(1) translate(90%, -70%)',
+            ...theme.typography.subtitle2,
+          }
+        },
+        !!rounded && {
+          "& .MuiBadge-badge": {
 
-                    }
-                },
-                !!rounded && {
-                    "& .MuiBadge-badge" : {
-
-                        borderRadius: '50%',
-                    }
-                }
-            ]}
-            {...otherProps}
-        >
-            {children}
-        </MuiBadge>
-    );
+            borderRadius: '50%',
+          }
+        }
+      ]}
+      {...otherProps}
+    >
+      {children}
+    </MuiBadge>
+  );
 };
 
-
-// export const StyledBadge = withStyles((theme:any) => ({
-//     badge: {
-//       border: (props) => (props.border && `2px solid ${theme.palette.background.paper}`) || 0,
-//       padding: '0 4px',
-//       backgroundColor: (props) => props.backgroundcolor || theme.palette.background.default,
-//       color: (props) => props.textcolor || theme.palette.background.default,
-//     },
-//   }))(Badge);
-  
 export default Badge;
