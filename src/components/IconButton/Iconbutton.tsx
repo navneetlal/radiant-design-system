67f0e1b39377da9292b0d1b6bd2cdc4927a31e56
Navneet Lal Gupta <navneetlalg@gmail.com>
2022-01-28 16:35:10 +0530

    refactor: components/iconbutton.tsx

--- src/components/IconButton/Iconbutton.tsx
+++ src/components/IconButton/Iconbutton.tsx
@@ -1,92 +1,81 @@
 import React from 'react';
 
-import {Button as MuiButton} from '@mui/material';
-import { useTheme } from '@mui/material';
+import MuiButton from '@mui/material/Button';
+import { useTheme } from '@mui/material/styles';
 
+import type { ButtonProps } from '@mui/material/Button';
 
-
-export interface IButtonProps {
-  variant?: 'outlined' | 'contained';
-  color?: "primary" | "secondary" | "error" | "warning" | "success" | "inherit" | "info";
-  size?: 'small' | 'medium' | 'large';
-  icon?: React.ReactElement;
+export interface IButtonProps extends ButtonProps {
+  color?: "primary" | "secondary" | "error" | "warning" | "success";
   [key: string]: any;
 }
 
-const IconButton = ({ variant, color, size, icon, ...otherProps }: IButtonProps) => {
+const IconButton = ({ variant = "contained", color = "primary", size = "medium", ...otherProps }: IButtonProps) => {
   const theme = useTheme();
   return (
     <MuiButton
       variant={variant}
       color={color}
-      sx = {[
-          {
-            minWidth: 32,
-            '& $text': {
-              maxWidth: 0,
-              overflow: 'hidden',
-              transition: 'all .2s ease-out',
-            },
-            '&:hover $text': {
-              maxWidth: 500,
-              transition: 'all .2s ease-in',
-              paddingLeft: 5,
-            },
+      sx={[
+        {
+          minWidth: 32,
+          '& $text': {
+            maxWidth: 0,
+            overflow: 'hidden',
+            transition: 'all .2s ease-out',
           },
-          variant === 'contained' && {
-            //@ts-ignore
-              background: theme.palette[color].main,
-              color : `#ffffff`,
-              '&:hover' : {
-                //@ts-ignore
-                  background : theme.palette[color].dark,
-                },
-              boxShadow : 'none !important'
+          '&:hover $text': {
+            maxWidth: 500,
+            transition: 'all .2s ease-in',
+            paddingLeft: 5,
           },
-          variant === 'outlined' && {
-            //@ts-ignore
-                color : theme.palette[color].main,
-                background : '#ffffff',
-                border : `1px solid`,
-                //@ts-ignore
-                borderColor : theme.palette[color].main,
-                '&:hover' : {
-                  //@ts-ignore
-                    background : theme.palette[color].contrastText
-                }
+        },
+        variant === 'contained' && {
+          background: theme.palette[color].main,
+          color: `#ffffff`,
+          '&:hover': {
+            background: theme.palette[color].dark,
           },
-          size === 'small' && {
-            width: '32px',
-            height: '32px',
-            padding: '8px',
-            '& svg': {
-              width: '16px',
-              height: '16px',
-            },
-          },
-          size === 'medium' && {
-            width: '40px',
-            height: '40px',
-            padding: '10px',
-            '& svg': {
-              width: '20px',
-              height: '20px',
-            },
-          },
-          size === 'large' && {
-            width: '48px',
-            height: '48px',
-            ...theme.typography.h4,
-            padding: '12px',
-            '& svg': {
-              width: '24px',
-              height: '24px',
-            },
+          boxShadow: 'none !important'
+        },
+        variant === 'outlined' && {
+          color: theme.palette[color].main,
+          background: '#ffffff',
+          border: `1px solid`,
+          borderColor: theme.palette[color].main,
+          '&:hover': {
+            background: theme.palette[color].contrastText
           }
-
-          
+        },
+        size === 'small' && {
+          width: '32px',
+          height: '32px',
+          padding: '8px',
+          '& svg': {
+            width: '16px',
+            height: '16px',
+          },
+        },
+        size === 'medium' && {
+          width: '40px',
+          height: '40px',
+          padding: '10px',
+          '& svg': {
+            width: '20px',
+            height: '20px',
+          },
+        },
+        size === 'large' && {
+          width: '48px',
+          height: '48px',
+          ...theme.typography.h4,
+          padding: '12px',
+          '& svg': {
+            width: '24px',
+            height: '24px',
+          },
+        }
       ]}
-      startIcon={icon}
       {...otherProps}
     >
     </MuiButton>
