0536cc73a0027549345c3e6af7ec611a45fc4a47
Navneet Lal Gupta <navneetlalg@gmail.com>
2022-01-31 13:20:58 +0530

    refactor: components/tooltip.tsx

--- src/components/Tooltip/Tooltip.tsx
+++ src/components/Tooltip/Tooltip.tsx
@@ -1,39 +1,25 @@
-import { Tooltip as MuiTooltip } from "@mui/material";
-import { useTheme } from "@mui/material";
 import React from "react";
 
-export interface IToolTipProps {
-  title: string;
+import MuiTooltip from "@mui/material/Tooltip";
+import { useTheme } from "@mui/material/styles";
+import type { TooltipProps } from "@mui/material/Tooltip";
+
+export interface ITooltipProps extends TooltipProps {
   children: JSX.Element;
   color: "light" | "dark";
   size: "small" | "medium";
-  placement:
-    | "bottom-start"
-    | "bottom"
-    | "left-end"
-    | "left-start"
-    | "left"
-    | "right-end"
-    | "right-start"
-    | "right"
-    | "top-end"
-    | "top-start"
-    | "top";
   [key: string]: any;
 }
 
-const ToolTip = ({
-  title,
+const Tooltip = ({
   children,
   color,
   placement,
   ...otherProps
-}: IToolTipProps) => {
+}: ITooltipProps) => {
   const theme = useTheme();
   return (
     <MuiTooltip
-    
-      title={title}
       placement={placement}
       arrow
       {...otherProps}
@@ -41,48 +27,39 @@ const ToolTip = ({
         tooltip: {
           sx: {
             backgroundColor:
-              //@ts-ignore
               color === "dark" ? theme.palette.primary.dark : "#ffffff",
             color: color === "dark" ? "#ffffff" : "#1A1A1A",
 
             height: otherProps.size === "medium" ? "18px" : "14px",
-            //@ts-ignore
             border: `1px solid ${theme.palette.primary[200]}`,
             borderRadius: "4px",
 
             minWidth: otherProps.size === "small" ? "35px" : "38px",
             textAlign: "center",
-            //justifyContent : 'center',
             ...(otherProps.size === "small"
               ? {
-                  ...theme.typography.body2,
-                }
+                ...theme.typography.body2,
+              }
               : {
-                  ...theme.typography.body1,
-                }),
-            padding: otherProps.size === "medium" ? "8px 16px" : "4px 8px", // Top Down  Left Right
+                ...theme.typography.body1,
+              }),
+            padding: otherProps.size === "medium" ? "8px 16px" : "4px 8px",
             ...(placement === 'top' && {
 
-              marginBottom : '4px !important',
+              marginBottom: '4px !important',
             }),
             ...(placement === 'bottom' && {
-              marginTop : '4px !important'
+              marginTop: '4px !important'
             })
-            
           },
         },
         arrow: {
           sx: {
-            //@ts-ignore
             color: color === "dark" ? theme.palette.primary.dark : "#ffffff",
             position: "absolute",
             top: otherProps.size === "medium" ? "34.2px" : "22.2px",
-            
-            
-            //marginTop : '4px',
             "&:before": {
               border: `.1px solid`,
-              //@ts-ignore
               borderColor: theme.palette.primary[200],
               borderRadius: "2px",
             },
@@ -95,4 +72,4 @@ const ToolTip = ({
   );
 };
 
-export default ToolTip;
+export default Tooltip;
