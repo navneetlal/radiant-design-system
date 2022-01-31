c4e6147fe5fdfcadb8d9f0266168723e8d881711
Siddhant Sharma <siddhant@intugine.com>
2022-01-31 18:24:31 +0530

    refactor components/multiselect

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -1,3 +1,5 @@
 // export { default as Chip } from './Chip';
 export { default as ArrowLeft } from './ArrowLeft';
 export { default as Checked } from './Checked';
+export {default as ChipIcon} from './Chip';
+export {default as AddIcon} from './Add';
